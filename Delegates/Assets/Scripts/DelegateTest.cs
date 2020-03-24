c7b886b12acbe3a36c68869a27dc670902bc21e1
Kostas <mpanakak@hotmail.com>
2020-03-24 20:38:25 +0200

    Delegates Session 1
    Simple Delegates test to see how they work

---
+++ Delegates/Assets/Scripts/DelegateTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class DelegateTest : MonoBehaviour
+{
+    public delegate void OnChangeColor(Color _clr);
+    public OnChangeColor onChangeColor;
+
+    private delegate void OnTaskEnd();
+    private OnTaskEnd onTaskEnd;
+    void Start()
+    {
+        onChangeColor = ColorChanger;
+        onChangeColor(Color.green);
+
+        onTaskEnd += Task1;
+        onTaskEnd += Task2;
+
+        try
+        {
+            onTaskEnd();
+        }
+        catch(NullReferenceException e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        if (onTaskEnd != null)
+            onTaskEnd();
+
+    }
+
+    private void Task1()
+    {
+        Debug.Log("task 1 completed");
+    }
+    public void Task2()
+    {
+        Debug.Log("task 2 completed");
+    }
+
+    private void ColorChanger(Color _color)
+    {
+        Debug.Log(_color);
+    }
+
+}
