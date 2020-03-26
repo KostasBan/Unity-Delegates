146314f7e293a7ff2a1dec5528f2bbb5ad425869
Kostas <mpanakak@hotmail.com>
2020-03-26 13:27:24 +0200

    Actions Session 6
    Actions are cleaner Delegetes

---
+++ Delegates/Assets/Scripts/ActionsTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ActionsTest : MonoBehaviour
+{
+    public static Action onTaskEnd;
+
+    public static Action<Color> changeColor;
+
+    private void Start()
+    {
+        onTaskEnd += Task;
+        onTaskEnd?.Invoke();
+
+        changeColor += ColorChanger;
+        changeColor?.Invoke(Color.green);
+    }
+
+    public void Task()
+    {
+        Debug.Log("task completed");
+    }
+
+    private void ColorChanger(Color _color)
+    {
+        Debug.Log(_color);
+    }
+}
